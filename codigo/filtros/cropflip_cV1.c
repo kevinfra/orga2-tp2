bf221fbadda8b781a3d9f31d39786d58bba5a1ff
Kevinfra <kevinfra94@gmail.com>
2016-04-26 20:21:05 -0300

    V2 de cropflip_c terminada

--- codigo/filtros/cropflip_cV1.c
+++ codigo/filtros/cropflip_cV1.c
@@ -18,18 +18,11 @@ void cropflip_c    (
 
 	// ejemplo de uso de src_matrix y dst_matrix (copia una parte de la imagen)
 
-	int finy = tamy+offsety-1;
-	int finx = tamx + offsetx;
-
-	int itDstY = 0;
-	for (int i = tamy+offsety+tamy-2; i >= finy; i--) {
-		int itDstX = 0;
-		for (int j = offsetx; j < finx+offsetx; j++) {
-			bgra_t *p_d = (bgra_t*) &dst_matrix[itDstY][itDstX * 4];
-			bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
+	for (int i = 0; i < tamy; i++) {
+		for (int j = 0; j < tamx; j++) {
+			bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
+			bgra_t *p_s = (bgra_t*) &src_matrix[tamy + offsety - i -1][(offsetx + j) * 4];
 			*p_d = *p_s;
-			itDstX++;
 		}
-		itDstY++;
 	}
 }
