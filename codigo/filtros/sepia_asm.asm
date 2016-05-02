76f88084f87d39f9e4c2b48794d374af021ff5ce
Kevinfra <kevinfra94@gmail.com>
2016-05-01 21:14:17 -0300

    sepia con cambios

--- codigo/filtros/sepia_asm.asm
+++ codigo/filtros/sepia_asm.asm
@@ -1,12 +1,14 @@
 section .data
 DEFAULT REL
 
-%define fruta 0
-Divisor : dd 1.0 ,0.5, 0.3 , 0.2
+%define null 0
+%define tam4pixels 16
+canalRojo: dd 0.5, 0, 0, 0
+canalVerde: dd 0.3, 0, 0, 0
+canalAzul: dd 0.2, 0, 0, 0
 
 section .text
 global sepia_asm
-
 ; void sepia_c    (
 ;     unsigned char *src, rdi
 ;     unsigned char *dst, rsi
@@ -16,98 +18,231 @@ global sepia_asm
 ;     int dst_row_size)		r9
 ; {
 sepia_asm:
-
-.StackFrame:
-push rbp
-mov rsp , rbp
-push r15 ; R15 = Contandor Filas
-push r14 ; R14 = Contador Filas
-push r13 ; R13 = Suma RGB
-push r12 ; R12 = Suma RGB
-push rbx
-xor r15 , r15
-xor r14 , r14
-xor r13 , r13
-xor r12 , r12
-xor rbx , rbx
-pxor xmm15, xmm15 ;Va a tener los valores para dividir
-pxor xmm14, xmm14 ;Valores en byte para tirar a memoria  
-pxor xmm13, xmm13
-pxor xmm12, xmm12
-pxor xmm11, xmm11
-pxor xmm10, xmm10
-pxor xmm9 , xmm9
-pxor xmm8 , xmm8
-pxor xmm7 , xmm7
-pxor xmm6 , xmm6
-pxor xmm5 , xmm5
-pxor xmm4 , xmm4 ; PlaceHolder
-pxor xmm3 , xmm3 ; Parte Alta
-pxor xmm2 , xmm2 ; Parte Baja
-pxor xmm1 , xmm1 ; XMM1 tendra los pixels originales
-pxor xmm0 , xmm0 ; XMM0 solo con 0 SIEMPRE  
-movdqu xmm15 , [Divisor]
-
-		.CicloFilas:
-		cmp r15 , rcx
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	xor rbx, rbx
+	xor r12, r12
+	pxor xmm15, xmm15
+	pxor xmm14, xmm14
+	
+	.forFilas:
+		cmp rbx, rcx
 		je .fin
 
-		.cicloColumnas:
-		cmp r14 , rdx
-		je .NextFila
-							; A ; R ; G ; B
-		.loadPixels:
-		movdqu xmm1 , [rdi] ; RDI = |p0|p1|p2|p3|
-		movdqu xmm2 , xmm1
-		movdqu xmm3 , xmm1
-		punpcklbw xmm2 , xmm0 ; XMM2 = P2 y P3 Exten
-		punpckhbw xmm3 , xmm0 ; XMM3 = P0 y P1 Ext
-		;XMM4-14 LIBRES 
-		.SumaLow: 
-		;Hago la suma de la otra parte
-		movdqu xmm4 , xmm2
-		psrldq xmm4 , 2 ; Shifteo 2 Bytes a Derecha
-		paddw xmm2 , xmm4
-		psrldq xmm4 , 2
-		paddw xmm2 , xmm4
-		;XMM2 Word 0 y 4 suma RGB ; Resto fruta podrida
-		pextrw r13w, xmm2 , 0 ; R13W = p3 (R+G+B)
-		pextrw r12w, xmm2 , 4 ; R12W = p2 (R+G+B) 
-		.SumaHigh:
-		movdqu xmm4 , xmm3
-		psrldq xmm4 , 2
-		paddw xmm3 , xmm4
-		psrldq xmm4 , 2 
-		paddw xmm3 , xmm4
-		pextrw ax , xmm3 , 0 ; AX = P1 (R+G+B)
-		pextrw bx , xmm3 , 4 ; BX = P0 (R+G+B)
+		.forCols:
+			cmp r12, rdx
+			jge .avanzarFila
 
-		.Extend_Double_Float_P0:
-		;XMM4 Disponible
-		pxor xmm4 , xmm4
-		pinsrw xmm4 , bx , 0
-		pinsrw xmm4 , bx , 2
-		pinsrw xmm4 , bx, 4
-		xor bx , bx
-		pinsrb bl , xmm0 , 15
-		pinsrb xmm4 , bx, 6
-		cvtdq2ps xmm5 , xmm4 ; Paso a Float
-		divps xmm5 , xmm4 ; Divido
-		CVTPS2DQ xmm4 , xmm5 ; Paso a Dword
-		PACKUSDW 					;Pack D_2_W	
+			.cargar4pixelsEnXmm1:
+				pxor xmm2, xmm2
+				pxor xmm3, xmm3
+				pxor xmm4, xmm4
+				pxor xmm5, xmm5
+				pxor xmm6, xmm6
+				pxor xmm7, xmm7
+				pxor xmm8, xmm8
+				pxor xmm9, xmm9
+				pxor xmm11, xmm11
+				pxor xmm12, xmm12
+				pxor xmm13, xmm13
+				pxor xmm14, xmm14
+				pxor xmm15, xmm15
+				movups xmm0, [rdi] ;ahora guardo una copia de lo que voy a modificar. xmm0 = |p0a|p0r|p0g|p0b|···|p3a|p3r|p3g|p3b| byte
+				movups xmm2, xmm0 ; carga los primeros 4 pixels en xmm1
+				movdqu xmm5, xmm0
+				punpcklbw xmm2, xmm3 ;expando el byte de cada canal a word en parte baja (2 a 3 pixel)xmm2 = |0 p2a | 0 p2r | 0 p2g | 0 p2b | 0 p3a | 0 p3r | 0 p3g | 0 p3b |
+				punpckhbw xmm5, xmm3 ;expando el byte de cada canal a word de la parte alta (pixels 0 y 1)
+				
+				;suma rgb parte baja
+				movdqu xmm3, xmm2
+				psrlq xmm3, 00001000b
+				psrlq xmm3, 00001000b; ; xmm3= |0 | 0-P2a | 0-p2r | 0-p2g | 0 | 0-p3a | 0-p3r | 0-p3g |
+				paddw xmm2, xmm3 ; xmm2=|p2a|p2a+p2r|p2r+p2g|p2g+p2b|p3a|p3a+p3r|p3r+p3g|p3g+p3b|
+				psrlq xmm3, 00001000b
+				psrlq xmm3, 00001000b ; xmm3=| 0 |	0	| 0-p2a | 0-p2r | 0 |	0	| 0-p3a | 0-p3r |
+				paddw xmm2, xmm3 ; xmm2=|p2a|p2a+p2r|p2r+g+a|p2g+b+r|p3a|p3a+p3r|p3r+g+a|p3g+b+r| --libero xmm3 y xmm4
+				
+				;suma rgb parte alta
+				movdqu xmm3, xmm5
+				psrlq xmm3, 00001000b
+				psrlq xmm3, 00001000b
+				paddw xmm5, xmm3
+				psrlq xmm3, 00001000b
+				psrlq xmm3, 00001000b
+				paddw xmm5, xmm3 ;queda en la parte baja de cada parte baja de cada dword en xmm5 la sumaRGB de los pixels 0 y 1
+									; --libero xmm3, xmm4
+									; tengo en xmm2 y xmm5 la sumaRGB de cada pixel en tamano word en la parte baja de cada dword
+				
+				;laburo con pixel3 (Parte baja de xmm2)
+				.pixel3:
+				pxor xmm4, xmm4
+				movdqu xmm3, xmm2
+				punpcklwd xmm3, xmm4 ;expando el word de cada canal a dword de la parte baja de xmm2 (pixel 3)
+				movdqu xmm4, xmm3
+				movdqu xmm6, xmm3
+						;dejo copiado xmm3 en xmm4 y xmm6, para hacer la multiplicacion por 5, 3 y 2 y luego 
+						
+				cvtdq2ps xmm7, xmm6
+				cvtdq2ps xmm8, xmm4
+				cvtdq2ps xmm9, xmm3
+				
+				mulps xmm7, [canalRojo]
+				mulps xmm8, [canalVerde]
+				mulps xmm9, [canalAzul]
+			
+				pxor xmm6, xmm6
+				punpcklwd xmm6, xmm2 ;tengo en la ultima dword de xmm6 a p3a
+				psrldq xmm6, 12d ;tengo en la dword mas baja de xmm6 a p3a
+				cvtdq2ps xmm3, xmm6
+				
+				shufps xmm7, xmm3, 01000100b ; xmm7 = | 0 |p3a| 0 |p3r|
+				shufps xmm9, xmm8, 01000100b ; xmm9 = | 0 |p3g| 0 |p3b|
+				shufps xmm9, xmm7, 10001000b ; xmm9 = |a|r|g|b|
+				cvtps2dq xmm15, xmm9
+
+				;tengo libres xmm3, xmm4, xmm6 ->
+				
+				;tengo en xmm2 los pixels 2 y 3. 3 está listo, así que repito lo anterior con xmm2 parte alta
+				pxor xmm3, xmm3
+				pxor xmm4, xmm4
+				pxor xmm6, xmm6
+				pxor xmm7, xmm7
+				pxor xmm8, xmm8
+				pxor xmm9, xmm9
+				
+				.pixel2:
+				pxor xmm4, xmm4
+				movdqu xmm3, xmm2
+				punpckhwd xmm3, xmm4 ;expando el word de cada canal a dword de la parte alta de xmm2 (pixel 2)
+				movdqu xmm4, xmm3
+				movdqu xmm6, xmm3
+						;dejo copiado xmm3 en xmm4 y xmm6, para hacer la multiplicacion por 5, 3 y 2 y luego 
+				
+				cvtdq2ps xmm7, xmm6 ;convierto sumaRGB dword a single floating point
+				cvtdq2ps xmm8, xmm4
+				cvtdq2ps xmm9, xmm3
+				
+				mulps xmm7, [canalRojo]
+				mulps xmm8, [canalVerde]
+				mulps xmm9, [canalAzul]
+				
+				punpckhwd xmm6, xmm2 ;tengo en la ultima dword de xmm7 a p2a
+				psrld xmm6, 12d;tengo en la dword mas baja de xmm6 a p2a
+				cvtdq2ps xmm3, xmm6
+				
+				shufps xmm7, xmm3, 01000100b ; xmm3 = | 0 |p2a| 0 |p2r|
+				shufps xmm9, xmm8, 01000100b ; xmm9 = | 0 |p2g| 0 |p2b|
+				shufps xmm9, xmm7, 10001000b ; xmm9 = |a|r|g|b|
+				
+				cvtps2dq xmm14, xmm9 ;|a|r|g|b|
+				
+				.empaquetarP2P3:
+				;tengo en xmm15 p3 listo y en xmm14 p2 listo empaquetado como dword. Los junto ambos para volver a tener todo listo en xmm2
+				packusdw xmm15, xmm14 ; xmm14 = |p2a|p2r|p2g|p2b|p3a|p3r|p3g|p3b|
+				movdqu xmm14, xmm15
+				
+				
+				;ahora hay que repetir el paso anterior con la parte alta de xmm1
+				pxor xmm15, xmm15
+				pxor xmm9, xmm9
+				pxor xmm8, xmm8
+				pxor xmm7, xmm7
+				pxor xmm6, xmm6
+				pxor xmm4, xmm4
+				pxor xmm3, xmm3
+				pxor xmm2, xmm2
+				
+				;al principio tenia los pixeles p1 y p0 en xmm5 en word. Lo muevo a xmm2 por comodidad.
+				movdqu xmm2, xmm5
+				.pixel1:
+				pxor xmm4, xmm4
+				movdqu xmm3, xmm2
+				punpcklwd xmm3, xmm4 ;expando el word de cada canal a dword de la parte baja de xmm2 (pixel 1)
+				movdqu xmm4, xmm3
+				movdqu xmm6, xmm3
+						;dejo copiado xmm3 en xmm4 y xmm6, para hacer la multiplicacion por 5, 3 y 2 y luego 
+				
+				cvtdq2ps xmm7, xmm6 ;convierto sumaRGB dword a single floating point
+				cvtdq2ps xmm8, xmm4
+				cvtdq2ps xmm9, xmm3
+				
+				mulps xmm7, [canalRojo]
+				mulps xmm8, [canalVerde]
+				mulps xmm9, [canalAzul]
+				
+				punpcklwd xmm6, xmm2 ;tengo en la ultima dword de xmm7 a p1a
+				psrld xmm6, 12d ;tengo en la dword mas baja de xmm6 a p1a
+				cvtdq2ps xmm3, xmm6
+				
+				shufps xmm7, xmm3, 01000100b ; xmm7 = | 0 |p1a| 0 |p1r|
+				shufps xmm9, xmm8, 01000100b ; xmm9 = | 0 |p1g| 0 |p1b|
+				shufps xmm9, xmm7, 10001000b ; xmm9 = |a|r|g|b|
+				cvtps2dq xmm3, xmm9
+
+				movdqu xmm15, xmm3 ;muevo xmm3 a xmm15 para dejar libre xmm3
+				;tengo en xmm15 |a|r|g|b| de pixel1
+				;tengo libres xmm3, xmm4, xmm6 ->
+				
+				;tengo en xmm2 los pixels 2 y 3. 3 está listo, así que repito lo anterior con xmm2 parte alta
+				pxor xmm3, xmm3
+				pxor xmm4, xmm4
+				pxor xmm6, xmm6
+				pxor xmm7, xmm7
+				pxor xmm8, xmm8
+				pxor xmm9, xmm9
+				
+				.pixel0:
+				pxor xmm4, xmm4
+				movdqu xmm3, xmm2
+				punpckhwd xmm3, xmm4 ;expando el word de cada canal a dword de la parte alta de xmm2 (pixel 0)
+				movdqu xmm4, xmm3
+				movdqu xmm6, xmm3
+						;dejo copiado xmm3 en xmm4 y xmm6, para hacer la multiplicacion por 5, 3 y 2 y luego 
+				
+				cvtdq2ps xmm7, xmm6 ;convierto sumaRGB dword a single floating point
+				cvtdq2ps xmm8, xmm4
+				cvtdq2ps xmm9, xmm3
+				
+				mulps xmm7, [canalRojo]
+				mulps xmm8, [canalVerde]
+				mulps xmm9, [canalAzul]
+				
+				punpckhwd xmm6, xmm2 ;tengo en la ultima dword de xmm7 a p0a
+				psrld xmm6, 12d ;tengo en la dword mas baja de xmm6 a p0a
+				cvtdq2ps xmm3, xmm6
+				
+				shufps xmm7, xmm3, 01000100b ; xmm3 = | 0 |p0a| 0 |p0r|
+				shufps xmm9, xmm8, 01000100b ; xmm9 = | 0 |p0g| 0 |p0b|
+				shufps xmm9, xmm7, 10001000b ; xmm9 = |a|r|g|b| 
+				
+				cvtps2dq xmm3, xmm9 ;|a|r|g|b|
+				movdqu xmm13, xmm3
+				
+				.empaquetarP0P1:
+				;tengo en xmm15 p1 listo y en xmm13 p0 listo empaquetado como dword. Los junto ambos para volver a tener todo listo en xmm2
+				packusdw xmm15, xmm13 ; xmm15 = |p0a|p0r|p0g|p0b|p1a|p1r|p1g|p1b|
+				
+				.empaquetarAByte:
+				packuswb xmm14, xmm15 ; xmm13 = |p0a|p0r|p0g|p0b|p1a|p1r|p1g|p1b|p2a|p2r|p2g|p2b|p3a|p3r|p3g|p3b|
+				movdqu [rsi], xmm14
+				
+				.avanzarIteradores:
+				add r12, 4 ;la cantidad de pixeles que veo por iteracion
+				add rdi, tam4pixels
+				add rsi, tam4pixels
+				jmp .forCols
+				
+				;empaqueto de WORD a Byte
+				
+		.avanzarFila:
+		xor r12, r12
+		add rbx, 1
+		jmp .forFilas
 		
-
-		.Extend_Double_Float_P1:
-
-		.Extend_Double_Float_P2:
-
-		.Extend_Double_Float_P3:
-
-		.NextFila:
-		
-
-.fin:
-pop r15
-pop r14
-pop rbp
-ret
+	.fin:
+	pop r12
+	pop rbx
+	pop rbp
+	ret
