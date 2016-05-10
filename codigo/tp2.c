b4abade259cf7ab6768438ce79acc51f8e9c0f3e
Kevinfra <kevinfra94@gmail.com>
2016-05-10 19:22:03 -0300

    prueba de comparacion por segmentation fault en maquinas labo

--- codigo/tp2.c
+++ codigo/tp2.c
@@ -38,10 +38,10 @@ int main( int argc, char** argv ) {
 	// Imprimo info
 	if (!config.nombre)
 	{
-//		printf ( "Procesando...\n");
-//		printf ( "  Filtro             : %s\n", config.nombre_filtro);
-//		printf ( "  Implementación     : %s\n", C_ASM( (&config) ) );
-//		printf ( "  Archivo de entrada : %s\n", config.archivo_entrada);
+		printf ( "Procesando...\n");
+		printf ( "  Filtro             : %s\n", config.nombre_filtro);
+		printf ( "  Implementación     : %s\n", C_ASM( (&config) ) );
+		printf ( "  Archivo de entrada : %s\n", config.archivo_entrada);
 	}
 
 	filtro_t *filtro = detectar_filtro(&config);
@@ -70,13 +70,12 @@ filtro_t* detectar_filtro(configuracion_t *config)
 void imprimir_tiempos_ejecucion(unsigned long long int start, unsigned long long int end, int cant_iteraciones) {
 	unsigned long long int cant_ciclos = end-start;
 
-//	printf("Tiempo de ejecución:\n");
-//	printf("  Comienzo                          : %llu\n", start);
-//	printf("  Fin                               : %llu\n", end);
-//	printf("  # iteraciones                     : %d\n", cant_iteraciones);
-//	printf("  # de ciclos insumidos totales     : %llu\n", cant_ciclos);
-//	printf("  # de ciclos insumidos por llamada : %.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
-printf("%.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
+	printf("Tiempo de ejecución:\n");
+	printf("  Comienzo                          : %llu\n", start);
+	printf("  Fin                               : %llu\n", end);
+	printf("  # iteraciones                     : %d\n", cant_iteraciones);
+	printf("  # de ciclos insumidos totales     : %llu\n", cant_ciclos);
+	printf("  # de ciclos insumidos por llamada : %.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
 }
 
 void correr_filtro_imagen(configuracion_t *config, aplicador_fn_t aplicador)
